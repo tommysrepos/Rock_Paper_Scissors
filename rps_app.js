a89cb26930de23d43b5b1a59e886faeeb8e7a57b
Thomas <tommyvalotta@gmail.com>
2023-01-17 23:11:57 -0500

    Created function playRound() that plays a single round of rock, paper, scissors with the paramaters playerSelection and computerSelection

--- rps_app.js
+++ rps_app.js
@@ -12,3 +12,29 @@ function getComputerChoice(){
     }
 }
 console.log(getComputerChoice(getRandomInt()));
+function playRound(playerSelection, computerSelection){
+   if (playerSelection == "Rock" && computerSelection == "Rock"){
+    return "Tie! You both chose rock.";
+   }else if (playerSelection == "Rock" && computerSelection == "Paper"){
+    return "You lose! Paper beats rock.";
+   } else if (playerSelection == "Rock" && computerSelection == "Scissors"){
+    return "You win! Rock beats scissors.";
+   }else if (playerSelection == "Paper" && computerSelection == "Rock"){
+    return "You win! Paper beats rock.";
+   }else if (playerSelection == "Paper" && computerSelection == "Paper"){
+    return "Tie! You both chose paper.";
+   }else if (playerSelection == "Paper" && computerSelection == "Scissors"){
+    return "You lose. Scissors beats paper."
+   }else if (playerSelection == "Scissors" && computerSelection == "Rock"){
+    return "You lose. Rock beats scissors."
+   }else if (playerSelection =="Scissors" && computerSelection == "Paper"){
+    return "You win! Scissors beats paper."
+   }else if(playerSelection == "Scissors" && computerSelection == "Scissors"){
+    return "Tie! You both chose scissors."
+   }else{
+    return "This is not possible."
+   }
+   }
+const playerSelection = "Paper";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
